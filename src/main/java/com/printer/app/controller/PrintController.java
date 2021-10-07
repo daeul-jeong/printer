699850cfa424735f73368e5d7207210e7920dc8a
Da Eul Jeong <elin.jeong@goodoc.co.kr>
2021-10-07 17:31:42 +0900

    add : request 받는 컨트롤러, req/res 받는 html 추가

--- src/main/java/com/printer/app/controller/PrintController.java
+++ src/main/java/com/printer/app/controller/PrintController.java
@@ -5,15 +5,33 @@ import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.io.IOException;
 import java.text.Collator;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Controller
-@RequiredArgsConstructor
 @Slf4j
 public class PrintController {
+
+    @RequestMapping("/")
+    public String index(
+            @RequestParam(value = "url", required = false) String url,
+            @RequestParam(value = "textType", required = false) String textType,
+            @RequestParam(value = "printUnit", required = false) String printUnit,
+            Model model
+    ) {
+
+        model.addAttribute("url", url);
+        model.addAttribute("textType", textType);
+        model.addAttribute("printUnit", printUnit);
+        log.info("@@@@@@ " + url, textType, printUnit);
+        return "/index";
+    }
 
 
 }
