65362159039f9a7f92b597f566471fe02570134c
Da Eul Jeong <elin.jeong@goodoc.co.kr>
2021-10-07 16:55:24 +0900

    init

---
+++ src/main/java/com/printer/app/controller/PrintController.java
@@ -0,0 +1,19 @@
+package com.printer.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Controller;
+import java.io.IOException;
+import java.text.Collator;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class PrintController {
+
+
+}
